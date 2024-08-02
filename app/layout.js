0f89ef32f8b7c0d3ab2a137cbd50785e944dd659
Humanshu <humanshujaglan@gmail.com>
2024-08-02 20:42:12 +0530

    fixed error in production server

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Quicksand } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import Main from "./Main";
+import Navbar from "./universal-components/Navbar";
+import Footer from "./universal-components/Footer";
 
 const quicksand = Quicksand({ subsets: ["latin"] });
 
@@ -17,8 +19,10 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={quicksand.className}>
-        <Main children={children}/>
-
+        <Navbar/>
+        {/* <Main children={children}/> */}
+        {children}
+        <Footer/>
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></Script>
       </body>
     </html>
